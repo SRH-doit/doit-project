a6d55c664149bf35700dac99d1355ffdfe3b29fd
NANONANDFLASH <68502584+NANONANDFLASH@users.noreply.github.com>
2023-06-08 01:11:54 +0900

    mongo db install

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
+const mongoURI =
+  "mongodb+srv://Admin:PaRRseqABOcfNMv8@cluster0.4boiy3r.mongodb.net/doitDB?retryWrites=true&w=majority";
+
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB Atlas connected");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB Atlas:", err);
+  });
 
 app.use("/public", express.static("public"));
 
